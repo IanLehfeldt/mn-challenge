0e5a0379c81e141dfcbb0dc5bf6fd2c602a8f002
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-12-15 16:35:34 -0600

    sourced in material via cdn, checked to see if it works, everything is sourced in with no errors as far

--- public/view/script/client.js
+++ public/view/script/client.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyAppController', [function() {
-
+myApp.controller('MyAppController', function() {
     console.log('MyApp and App Controller is loaded.');
+    const vm = this;
     
-}]);
+});
---
+++ public/view/script/service.js
@@ -0,0 +1,5 @@
+myApp.service('MyAppService', function ($http){
+console.log('MyApp Service loaded.');
+const vm = this;
+
+});
