1baab16908fc5b82695535aa87dd73a97f1dc01e
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-12-15 16:20:55 -0600

    cleaned up some sources, confirmed that angular was sourced in

--- public/server/app.js
+++ public/server/app.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 5000;
 
+app.use(express.static('public/view'));
+app.use(bodyParser.json());
+
 app.listen(port, function() {
     console.log(`Listening on port: ${port}`);
-    
 });
