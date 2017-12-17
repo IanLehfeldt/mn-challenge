e222f40b35a9c0cdae91f6f45d421758374662a8
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-12-16 22:49:41 -0600

    set up a basic form to handle input fields, submit button added, submit button clicks and takes in information

--- public/view/script/client.js
+++ public/view/script/client.js
@@ -3,5 +3,26 @@ var myApp = angular.module('myApp', ['ngMaterial']);
 myApp.controller('MyAppController', function() {
     console.log('MyApp and App Controller is loaded.');
     const vm = this;
+
+    vm.contact = {};
     
+    vm.senatorList = [
+        "Senator A",
+        "Senator B",
+        "Senator C",
+        "Senator D",
+        "Senator E"
+    ];
+
+    vm.districtList = [
+        "District 1",
+        "District 2",
+        "District 3",
+        "District 4",
+        "District 5"
+    ];
+
+    vm.submitMessage = (contact) => {
+        console.log('Submit button clicked: ', contact);
+    }
 });
