fae28228cc65699cf924b9ba9d5037172bb73be8
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-12-16 20:12:45 -0600

    sourced in vendors to project, material definitely works now

--- public/view/script/client.js
+++ public/view/script/client.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMaterial']);
 
 myApp.controller('MyAppController', function() {
     console.log('MyApp and App Controller is loaded.');
